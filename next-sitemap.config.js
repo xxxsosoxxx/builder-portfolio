cec6cc5da0c22ed5a66115450ce342fcb71d3e32
xxxsosoxxx <contact.souheila@gmail.com>
2025-07-22 20:42:37 +0200

    create site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+  siteUrl: 'https://www.souheilasaid.com',
+  generateRobotsTxt: true,
+};
+// This configuration file is used to generate a sitemap for the website.
+// It specifies the site URL, enables the generation of a robots.txt file,  
