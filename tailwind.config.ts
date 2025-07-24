40cf5bcf9a31bb134a1727ca367bfbdcc35123ac
xxxsosoxxx <contact.souheila@gmail.com>
2025-07-24 16:01:29 +0200

    Add Dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/**/*.{ts,tsx}"],
+  content: ["./client/**/*.{ts,js,jsx,tsx}"],
   prefix: "",
   theme: {
     container: {
