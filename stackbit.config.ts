63bf63cdeaa23071d8c870764f9d19b99e67acd5
xxxsosoxxx <contact.souheila@gmail.com>
2025-07-27 02:18:03 +0200

    Install stackbit

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,35 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  stackbitVersion: "~0.6.0",
+  nodeVersion: "18",
+  ssgName: "custom",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [
+            { name: "title", type: "string", required: true }
+          ]
+        }
+      ]
+    })
+  ],
+  siteMap: ({ documents }) => {
+  return documents.map((document) => {
+    return {
+      stableId: document.id,
+      urlPath: `/${document.modelName.toLowerCase()}/${document.id}`,
+      document,
+      isHomePage: document.id === "home" // optionnel
+    };
+  });
+}
+});
